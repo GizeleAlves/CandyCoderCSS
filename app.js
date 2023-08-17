0713feb66f2bb7de406fc4d80f3c5f36b9139b00
GizeleAlves <gizele0alves@gmail.com>
2023-08-17 01:11:32 -0300

    adicionando os ícones dos botões

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const botaoTeste = document.getElementById("teste") //selecionar o botão pelo id
+const botaoVoltar = document.getElementById("voltar");
+const botaoProximo = document.getElementById("proximo");
+const resposta = document.getElementById("respostaCorreta");
 
 function addCSS (){
     const textAreaElemento = document.querySelector('textarea')
     const css = textAreaElemento.value
+    if(css.trim() === ''){
+        alert("Ops! Tente digitar um comando CSS no editor primeiro!");
+    }else{
     const frente = document.querySelector('#frente') 
     frente.style.cssText = css
-    console.log(frente)
+    console.log(frente)}
 }
 
-botaoTeste.addEventListener('click', addCSS)
 
-const botaoVoltar = document.getElementById("voltar");
+botaoTeste.addEventListener('click', addCSS)
 
 botaoVoltar.addEventListener("click", function() {
     window.history.back();
